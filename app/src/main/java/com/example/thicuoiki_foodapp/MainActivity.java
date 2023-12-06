7d8970e6a62346d230943e1a52d135e3d0a470d8
dtquoc1202 <quoccccc36@gmail.com>
2023-12-06 14:56:19 +0700

    up

---
+++ app/src/main/java/com/example/thicuoiki_foodapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.thicuoiki_foodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.foodorder.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
