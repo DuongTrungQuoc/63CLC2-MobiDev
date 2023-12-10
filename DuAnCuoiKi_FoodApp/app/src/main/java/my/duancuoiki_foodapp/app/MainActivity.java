986dbffa924eff115ffcce568c5268a3e3e82adc
dtquoc1202 <quoccccc36@gmail.com>
2023-12-11 01:04:50 +0700

    up

--- DuAnCuoiKi_FoodApp/app/src/main/java/my/duancuoiki_foodapp/app/MainActivity.java
+++ DuAnCuoiKi_FoodApp/app/src/main/java/my/duancuoiki_foodapp/app/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -26,8 +28,16 @@ public class MainActivity extends AppCompatActivity {
         imageView.animate().alpha(1f).setDuration(1000).setListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
-                textView.animate().alpha(1f).setDuration(800);
+                textView.animate().alpha(1f).setDuration(1000);
             }
         });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,MainMenu.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
     }
 }
